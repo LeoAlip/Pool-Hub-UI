dde16fe408062c8f7c65d7ca9203c93130a7e413
LeoAlip <aliffedtri.sohimin01@gmail.com>
2026-04-09 17:43:23 +0800

    Enhance notification UI layout and styling
    Added UI elements and adjusted positions and sizes for better layout.

--- NotiSource.lua
+++ NotiSource.lua
@@ -49,15 +49,20 @@ function NGuiHub:CreateWindow(title, gameName)
 
 	local Frame = Instance.new("Frame")
 	local Header = Instance.new("Frame")
+	local slice = Instance.new("Frame")
 	local Title = Instance.new("TextLabel")
 	local GameName = Instance.new("TextLabel")
 	local Message = Instance.new("TextLabel")
 	local CloseButton = Instance.new("TextButton")
 
+	local UICorner1 = Instance.new("UICorner")
+
+	local UIStroke1 = Instance.new("UIStroke")
+
 	GameName.Name = "GameNameLabel"
 	GameName.AnchorPoint = Vector2.new(0.5, 0.5)
 	GameName.Size = UDim2.new(0, 75, 0, 45)
-	GameName.Position = UDim2.new(0.45, 0, 0.2, 0)
+	GameName.Position = UDim2.new(0.45, 0, 0.5, 0)
 	GameName.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 	GameName.BackgroundTransparency = 1
 	GameName.Text = gameName
@@ -88,12 +93,23 @@ function NGuiHub:CreateWindow(title, gameName)
 		notifiedCache[id] = true
 		
 		Frame.Name = "NotiFrame"
-		Frame.Size = UDim2.new(0, 350, 0, 115)
-		Frame.Position = UDim2.new(1.2, 0, 0.9, 0)
+		Frame.Size = UDim2.new(0, 350, 0, 125)
+		Frame.Position = UDim2.new(1.05, 0, 0.85, 0)
 		Frame.BackgroundColor3 = themes.FrameColor
-		Frame.BackgroundTransparency = 0
+		Frame.BackgroundTransparency = 0.05
 		Frame.Visible = false
 		Frame.Parent = Gui
+
+		UICorner1.Name = "UICorner1"
+		UICorner1.CornerRadius = Vector2.new(0, 12.5)
+		UICorner1.Parent = Frame
+
+		UIStroke1.Name = "UIStroke1"
+		UIStroke1.ApplyStrokeMode = "Border"
+		UIStroke1.Color = themes.BackgroundColor
+		UIStroke1.LineJoinMode = "Round"
+		UIStroke1.Thickness = 1.5
+		UIStroke1.Parent = Frame
 
 		Header.Name = "NotiHeader"
 		Header.AnchorPoint = Vector2.new(0.5, 0.5)
@@ -104,10 +120,19 @@ function NGuiHub:CreateWindow(title, gameName)
 		Header.Visible = true
 		Header.Parent = Frame
 
+		slice.Name = "slice"
+		slice.AnchorPoint = Vector2.new(0.5, 0.5)
+		slice.Size = UDim2.new(0.96, 0, 0, 5)
+		slice.Position = UDim2.new(0.5, 0, 1, 0)
+		slice.BackgroundColor3 = themes.BackgroundColor
+		slice.BackgroundTransparency = 0
+		slice.Visible = true
+		slice.Parent = Header
+
 		Title.Name = "TitleLabel"
 		Title.AnchorPoint = Vector2.new(0.5, 0.5)
 		Title.Size = UDim2.new(0, 100, 0, 45)
-		Title.Position = UDim2.new(0.15, 0, 0.2, 0)
+		Title.Position = UDim2.new(0.175, 0, 0.5, 0)
 		Title.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 		Title.BackgroundTransparency = 1
 		Title.Text = title
@@ -121,15 +146,15 @@ function NGuiHub:CreateWindow(title, gameName)
 		
 		Message.Name = "MessageLabel"
 		Message.AnchorPoint = Vector2.new(0.5, 0.5)
-		Message.Size = UDim2.new(1, 0, 0.8, 0)
-		Message.Position = UDim2.new(0.5, 0, 0.7, 0)
+		Message.Size = UDim2.new(0.95, 0, 0.775, 0)
+		Message.Position = UDim2.new(0.5, 0, 0.625, 0)
 		Message.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 		Message.BackgroundTransparency = 1
 		Message.Text = message
 		Message.RichText = true
 		Message.TextColor3 = themes.TextColor
 		Message.Font = Enum.Font.SourceSansSemibold
-		Message.TextSize = 22.000
+		Message.TextScaled = true
 		Message.ZIndex = 2
 		Message.TextXAlignment = Enum.TextXAlignment.Left
 		Message.Parent = Frame
@@ -157,7 +182,7 @@ function NGuiHub:CreateWindow(title, gameName)
 			Frame,
 			TweenInfo.new(0.7, Enum.EasingStyle.Quad, Enum.EasingDirection.In),
 			{
-				Position = UDim2.new(0.9, 0, 0.9, 0)
+				Position = UDim2.new(0.75, 0, 0.85, 0)
 			}
 		)
 		
